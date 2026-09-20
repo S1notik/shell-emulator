fec46438fabe1843388a2f4aea0d726e35164661
S1notik <semn.osipov.2007@bk.ru>
2026-09-20 16:37:23 +0300

    refactor: изменён формат вывода аргументов на кавычки

--- src/commands/LsCommand.java
+++ src/commands/LsCommand.java
@@ -2,12 +2,15 @@ package commands;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LsCommand implements Command {
 
     @Override
     public String execute(List<String> args) {
-        return "ls " + args;
+        return "ls " + args.stream()
+                .map(a -> "\"" + a + "\"")
+                .collect(Collectors.joining(", "));
     }
 
 }
