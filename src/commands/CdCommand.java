f5e94adf0347e930ef48ef18ff337dec6cfed23e
S1notik <semn.osipov.2007@bk.ru>
2026-09-20 16:37:23 +0300

    refactor: изменён формат вывода аргументов на кавычки

--- src/commands/CdCommand.java
+++ src/commands/CdCommand.java
@@ -1,11 +1,14 @@
 package commands;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CdCommand implements Command {
 
     @Override
     public String execute(List<String> args) {
-        return "cd " + args;
+        return "cd " + args.stream()
+                .map(a -> "\"" + a + "\"")
+                .collect(Collectors.joining(", "));
     }
 }
