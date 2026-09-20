1925e1eb324c6d1102a7eb4feda34712709d3a9f
S1notik <semn.osipov.2007@bk.ru>
2026-09-20 16:26:51 +0300

    feat: реализован разбор строки на команду и аргументы

--- src/parser/CommandParser.java
+++ src/parser/CommandParser.java
@@ -1,10 +1,20 @@
 package parser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CommandParser {
 
     public ParsedCommand parse(String line) throws ParseException {
-        return new ParsedCommand("", new ArrayList<String>());
+        if (line.isBlank()) {
+            return new ParsedCommand("", new ArrayList<>());
+        }
+        String[] splitLine = line.trim().split(" ");
+        String command = splitLine[0];
+        List<String> args = new ArrayList<>();
+        for (int i = 1; i < splitLine.length; i++) {
+            args.add(splitLine[i]);
+        }
+        return new ParsedCommand(command, args);
     }
 }
