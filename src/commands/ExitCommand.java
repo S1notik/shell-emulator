ad6b1d8dc43c213ca4c572ed40344d83bd2fa005
S1notik <semn.osipov.2007@bk.ru>
2026-09-25 05:14:59 +0300

    feat: добавлена проверка аргументов в командах exit и cd

--- src/commands/ExitCommand.java
+++ src/commands/ExitCommand.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class ExitCommand implements Command {
     @Override
     public String execute(List<String> args) {
+        if (!args.isEmpty()) {
+            return "exit: команда не принимает аргументов";
+        }
         System.exit(0);
         return "";
     }
